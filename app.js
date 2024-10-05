f98b0e25747ea19a8318de4e99d8b558651db9c0
Yedeli Ahati <yahati1@sheffileld.ac.uk>
2024-10-05 20:19:08 +0100

    Add GownGrad project details and update Test1 and Test2 page content

--- app.js
+++ app.js
@@ -13,8 +13,35 @@ app.get('/', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-    res.render('projects', { title: 'Projects' });
+
+    const projects = [
+        {
+            name: 'Android app for Graduation Dress Hire ',
+            tech: 'Kotlin, Jetpack Compose, Navigation, Reposition, ROOM, Hilt, ViewModel, Firebase Authentication, Firestore',
+            summary: 'Say goodbye to the stress of graduation gown shopping; GownGrad is your one-stop solution for renting the perfect attire for your special day! This Android app simplifies the process, allowing users to browse gowns based on their university and degree, manage orders, input body measurements, and handle payments—all from their mobile devices. Whether anonymous or registered, users can easily modify or delete orders. Make your graduation day special with GownGrad!',
+            img: '/images/GownGrad.png',
+            link: '/test1'
+        },
+        {
+            name: 'Android app for Graduation Dress Hire ',
+            tech: 'Kotlin, Jetpack Compose, Navigation, Reposition, ROOM, Hilt, ViewModel, Firebase Authentication, Firestore',
+            summary: 'Say goodbye to the stress of graduation gown shopping; GownGrad is your one-stop solution for renting the perfect attire for your special day! This Android app simplifies the process, allowing users to browse gowns based on their university and degree, manage orders, input body measurements, and handle payments—all from their mobile devices. Whether anonymous or registered, users can easily modify or delete orders. Make your graduation day special with GownGrad!',
+            img: '/images/GownGrad.png',
+            link: '/test2'
+        },
+    ];
+
+    res.render('projects', { title: 'Projects', projects: projects });
 });
+
+app.get('/test1', (req, res) => {
+    res.render('test1', { title: 'Android App for Graduation Dress Hire' });
+});
+
+app.get('/test2', (req, res) => {
+    res.render('test2', { title: 'Android App for Graduation Dress Hire' });
+});
+
 
 app.get('/resume', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'Resume.pdf'));
